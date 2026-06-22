707dfa29d47c5a03988ce6b025bacb148acb382c
Abhinav Jayesh <abhinav.in.mail@gmail.com>
2026-06-22 14:05:48 +0200

    adding another file to see

---
+++ nothing.py
@@ -0,0 +1,26 @@
+import os
+import time
+
+"""
+This is a useless file
+"""
+
+print("this is a useless file")
+
+time.sleep(0.3)
+
+def biggest(a,b):
+    if a > b:
+        return a
+    else:
+        print("a and b could be equal too, but here we consider that b is bigger")
+        return b
+
+def multiply(a,b)
+    return a*b
+
+for i in range(1000):
+    if i % 2 == 0:
+        print("ajsd;flja;dfj")
+    else:
+        print("")
